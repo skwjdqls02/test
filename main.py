f18a5c2459f891199914ada8a2ae6da57dd587ab
Na JeongBin <na929786@gmail.com>
2024-12-04 23:23:27 +0900

    선택 출력에서 전체 출력 삭제
    선택 부분에서 전체 출력 부분을 삭제. 당첨 번호, 보너스 중에만 선택적으로 출력 가능.

--- main.py
+++ main.py
@@ -6,16 +6,13 @@ View.view()
 button = True
 
 while(button):
-    print("1. 전체 출력 2. 당첨번호만 출력 3. 보너스만 출력 4. 종료")
+    print("1. 당첨번호만 출력 2. 보너스만 출력 3. 종료")
     choice = int(input("선택 : "))
     if choice == 1:
         print(View.EditFile.edit_lotto_num)
-        print(View.EditFile.bonus_num)
     elif choice == 2:
-        print(View.EditFile.edit_lotto_num)
-    elif choice == 3:
         print(View.EditFile.bonus_num)
-    elif choice == 4:
+    elif choice == 3:
         print("bye")
         button = False
     else:
