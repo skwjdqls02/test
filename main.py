a1e0654c4cd0bd4dc897b0def46b23608a7a5fff
Na JeongBin <na929786@gmail.com>
2024-12-04 23:08:56 +0900

    로그인 중간 멈춤 장치 개발
    로그인 시 틀리면 맞출때까지 루프가 도는데 아이디에 0을 입력하면 바로 루프가 멈춤.

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ View.view()
 
 id = '1234'
 pw = '5678'
-while(True):
+button = True
+while(button):
     input_id = input("ID : ")
     input_pw = input("PW : ")
+    if(input_id == '0'):
+        break
     if(input_id == id and input_pw == pw):
         print("Success")
         print(View.EditFile.edit_lotto_num)
