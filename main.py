6f9bae04fa579a4a6887fa967a0541ac0154408a
Na JeongBin <na929786@gmail.com>
2024-12-04 23:18:03 +0900

    출력 내용을 선택 할 수 있는 기능 개발
    1. 전체 출력 2. 당첨번호만 출력 3.보너스만 출력 4. 종료 이외의 숫자는 다시 입력하도록함.
    1 = 당첨번호 + 보너스 배열 출력
    2 = 당첨번호배열만 출력
    3 = 보너스 배열만 출력
    4 = bye 출력 후 루프 탈출
    이외 = try again 출력 다시 루프

--- main.py
+++ main.py
@@ -2,5 +2,21 @@ import View
 
 View.view()
 
-print(View.EditFile.edit_lotto_num)
-print(View.EditFile.bonus_num)
+
+button = True
+
+while(button):
+    print("1. 전체 출력 2. 당첨번호만 출력 3. 보너스만 출력 4. 종료")
+    choice = int(input("선택 : "))
+    if choice == 1:
+        print(View.EditFile.edit_lotto_num)
+        print(View.EditFile.bonus_num)
+    elif choice == 2:
+        print(View.EditFile.edit_lotto_num)
+    elif choice == 3:
+        print(View.EditFile.bonus_num)
+    elif choice == 4:
+        print("bye")
+        button = False
+    else:
+        print("try again!")
