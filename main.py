22bf8b81713e442692a890c6f59228b0be0c911a
Na JeongBin <na929786@gmail.com>
2024-12-04 23:07:04 +0900

    로그인 기능 추가
    while으로 아이디와 비번을 묻고 둘 다 맞다면 조+당첨번호, 보너스 정보 배열을 출력함. 틀리면 맞을때까지 입력.

--- main.py
+++ main.py
@@ -2,5 +2,15 @@ import View
 
 View.view()
 
-print(View.EditFile.edit_lotto_num)
-print(View.EditFile.bonus_num)
+id = '1234'
+pw = '5678'
+while(True):
+    input_id = input("ID : ")
+    input_pw = input("PW : ")
+    if(input_id == id and input_pw == pw):
+        print("Success")
+        print(View.EditFile.edit_lotto_num)
+        print(View.EditFile.bonus_num)
+        break
+    else:
+        print("Retry!")
